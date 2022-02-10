4c7c381f366762ca45a86e5c1a472d94ef6d9034
TNFH12 <tanianickoll11@gmail.com>
2022-02-09 22:36:59 -0500

    Creacion de menu

--- Taller_parejas/src/taller_parejas/Taller_parejas.java
+++ Taller_parejas/src/taller_parejas/Taller_parejas.java
@@ -4,6 +4,10 @@
  * and open the template in the editor.
  */
 package taller_parejas;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author tania
@@ -14,7 +18,50 @@ public class Taller_parejas {
      */
     public static void main(String[] args) {
         
-        
+        Scanner sca = new Scanner(System.in);
+        boolean exit = false;
+        int option; //Guardaremos la opcion del usuario
+
+        /**
+        * usuaremos un do while para el menu.
+        */
+         do{
+ 
+            System.out.println("1. Filtra canciones del mismo genero");
+            System.out.println("2. Filtrar canciones en el mismo año");
+            System.out.println("3. Ordenar por fecha");
+            System.out.println("4. Ordenar por duracio");
+            System.out.println("5. Salir");
+ 
+            try {
+ 
+                System.out.println("Escribe una de las opciones");
+                option = sca.nextInt();
+ 
+                switch (option) {
+                    case 1:
+                       
+                        break;
+                    case 2:
+                     
+                        break;
+                    case 3:
+                     
+                        break;
+                    case 4:
+                     
+                        break;
+                    case 5:
+                        exit = true;
+                        break;
+                    default:
+                        System.out.println("Solo números entre 1 y 5");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes insertar un número");
+                sca.next();
+            }
+        }while (!exit);
         
     }
     
